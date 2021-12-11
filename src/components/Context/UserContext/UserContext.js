71d64463f4595eb944daaa377d4a2dbfdc752cdb
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2021-12-11 01:21:59 -0300

    modificaciones en el login

--- src/components/Context/UserContext/UserContext.js
+++ src/components/Context/UserContext/UserContext.js
@@ -3,15 +3,15 @@ import React, { createContext, useContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserContextProvider = ({
-    acrossCount,
-    setAcrossCount,
+    signedUser,
+    setSignedUser,
     children,
 }) => {
-    const [signedUser, setSignedUser] = useState({
+    /*  const [signedUser, setSignedUser] = useState({
         user: {
             displayName: "",
         },
-    });
+    }); */
 
     return (
         <UserContext.Provider value={{ setSignedUser, signedUser }}>
